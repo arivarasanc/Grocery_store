aa1c189b5769045f8359ef5647ce64a6748c268a
arivarasan <arivarsan.chandru@yahoo.co.in>
2017-06-29 16:46:15 +0530

    Program.

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,4 +1,4 @@
-class Grocery_Store  
+class Grocery_Store
   Items = {'milk' => 1, 'bread' => 2, 'banana' => 3, 'apple' => 4}
   Unit_price = {1 => 3.97, 2 => 2.17, 3 => 0.99, 4 => 0.89}
   Offer_price = {1 => 5.00, 2 => 6.00, 3 => 0.99, 4 => 0.89}
@@ -7,8 +7,8 @@ class Grocery_Store
   def items_price_list
     puts 'This week pricing list'
     puts 'Item         Unit Price         Sale Price'
-    puts '------------------------------------------'
-    Items.each{|key, value| puts key.to_s + '         ' + '$' + Unit_price[value].to_s + '         ' + Offer_quantity[value].to_s + ' for $'+ Offer_price[value].to_s }
+    puts '-------------------------------------'
+    Items.each{|key, value| puts key.to_s + '        ' + '$' + Unit_price[value].to_s + '           ' + Offer_quantity[value].to_s + ' for $'+ Offer_price[value].to_s }
   end
 
   def get_input
@@ -23,22 +23,28 @@ class Grocery_Store
     items_list.each{|item| item_count[item.strip.downcase] += 1}
     puts 'your purchase list is ' + item_count.to_s
     final_bill = Hash.new(0)
+    actual_total = 0
     for each_item in item_count
       item = Items[each_item[0]]
       qty = each_item[1]
       if !item.nil?
-        final_bill[each_item[0]] = (each_item[1] > Offer_quantity[item]) ? (((qty%Offer_quantity[item]) * Offer_price[item]) + ((qty/Offer_quantity[item])*Unit_price[item])) : (qty*Unit_price[item])
+        final_bill[each_item[0]] = (each_item[1] > Offer_quantity[item]) ? (((qty/Offer_quantity[item]) * Offer_price[item]) + ((qty%Offer_quantity[item])*Unit_price[item])) : (qty*Unit_price[item])
+        actual_total += qty*Unit_price[item]
       else
         final_bill[each_item[0]] = "Stock Not Available"
       end
     end
+    existing_total = final_bill.inject(0) { |total, (k, v)| (total.to_f + v.to_f).round(2)}
 
     puts 'Item      Quantity       Total Price'
     puts '-------------------------------------'
     final_bill.each{|k, v| puts k + '        ' + item_count[k].to_s + '           ' + '$'+ v.to_s }
     puts ' ------------------------------------'
-    puts 'Total                   ' + final_bill.inject(0) { |total, (k, v)| (total.to_f + v.to_f).round(2).to_s}  end
+    puts 'Total                   ' + existing_total.to_s
+    puts 'Today you saved $' + (actual_total - existing_total).round(2).to_s
+  end
 end
 
-purchase = Grocery_Store.newpurchase.items_price_list
+purchase = Grocery_Store.new
+purchase.items_price_list
 purchase.get_input
